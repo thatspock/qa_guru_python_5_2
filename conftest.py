19cf88ef61abfeda1a020edf26e39e50509a68e6
Some Name <thatspock@gmail.com>
2023-04-06 14:29:31 +0700

    replace maximize window to fixed size

--- conftest.py
+++ conftest.py
@@ -3,5 +3,6 @@ from selene import browser
 
 @pytest.fixture()
 def open_browser():
-    browser.open('https://google.com').config.driver.maximize_window()
-    # browser.driver.set_window_size(1024, 768)
+    # browser.open('https://google.com').config.driver.maximize_window()
+    browser.open('https://google.com')
+    browser.driver.set_window_size(1440, 900)
